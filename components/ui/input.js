9afb334adc17181ce49e017f13c268a48dfa14d4
okanoeztuerk <134276552+okanoeztuerk@users.noreply.github.com>
2025-04-30 23:36:47 +0200

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,9 +1,1 @@
-
-export function Input(props) {
-  return (
-    <input
-      className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-black"
-      {...props}
-    />
-  );
-}
+export function Input(props) { return <input className='border rounded px-3 py-2 w-full' {...props} />; }
